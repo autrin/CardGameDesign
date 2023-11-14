183f9f6a8224be0890adcfb8176310e64b1ce5b2
autrin <autrinhakimi@gmail.com>
2023-11-14 17:19:56 -0600

    fixed playercountbutton

--- src/main/java/coms362/cards/model/PlayerCountButton.java
+++ src/main/java/coms362/cards/model/PlayerCountButton.java
@@ -1,15 +1,13 @@
 package coms362.cards.model;
 
 import coms362.cards.events.inbound.SetQuorumEvent;
-import coms362.cards.events.inbound.DealEvent;
 
 public class PlayerCountButton extends VariableButton {
 
     private static String kSelectorBase;
-    private static String kIds = SetQuorumEvent.kId + DealEvent.kId;
 
-    public PlayerCountButton(String label, Location location) { //TODO
-        super(kSelectorBase, kIds, label, location);
+    public PlayerCountButton(String label, Location location) {
+        super(kSelectorBase + label, SetQuorumEvent.kId, label, location);
     }
 
 }
