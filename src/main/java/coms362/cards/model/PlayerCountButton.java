c5ce59a2e4fbc47cab2253dc53073d30e2c3e8b4
autrin <autrinhakimi@gmail.com>
2023-11-14 16:40:21 -0600

    fixed the double class head

--- src/main/java/coms362/cards/model/PlayerCountButton.java
+++ src/main/java/coms362/cards/model/PlayerCountButton.java
@@ -1,15 +1,16 @@
 package coms362.cards.model;
 
 import coms362.cards.events.inbound.SetQuorumEvent;
+import coms362.cards.events.inbound.DealEvent;
 
 public class PlayerCountButton extends VariableButton {
 
     private String kSelectorBase;
+    String kId = SetQuorumEvent.kId + DealEvent.kId;
 
     public PlayerCountButton(String label, Location location) {
-        String s = kSelectorBase + label;
-        String = 
-        super(s, SetQuorumEvent.kId, label, location);
+        super(sAndL, kId, location);
+        String sAndL = kSelectorBase + label;
         // TODO Auto-generated constructor stub
     }
 
