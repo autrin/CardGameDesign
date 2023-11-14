af9352b90e898ceee345453c8ed804d9cef2c782
autrin <autrinhakimi@gmail.com>
2023-11-14 15:11:38 -0600

    Created playercount button and selectgamebutton

---
+++ src/main/java/coms362/cards/model/PlayerCountButton.java
@@ -0,0 +1,12 @@
+package coms362.cards.model;
+
+public class PlayerCountButton extends VariableButton {
+
+    private String kSelectorBase;
+
+    public PlayerCountButton(String selector, String evtName, String label, Location location) {
+        super(selector, evtName, label, location);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/main/java/coms362/cards/model/SelectGameButton.java
@@ -0,0 +1,12 @@
+package coms362.cards.model;
+
+public class SelectGameButton extends VariableButton {
+    
+    private String kSelectorBase;
+
+    public SelectGameButton(String selector, String evtName, String label, Location location) {
+        super(selector, evtName, label, location);
+        // TODO Auto-generated constructor stub
+    }
+
+}
